0f59207478d6f6ad416a856bc6c62583f03028a7
illuzio <illuzio571@yahoo.com>
2019-03-16 17:10:49 -0400

    Changed Images

--- BullyMe/app/src/main/java/net/somee/bullyme/MainActivity.java
+++ BullyMe/app/src/main/java/net/somee/bullyme/MainActivity.java
@@ -90,17 +90,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_questionnaire) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_schedule) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_stop) {
 
         }
 
