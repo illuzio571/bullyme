f9d8c0203a087a83f605406feb18cee238216b61
illuzio <illuzio571@yahoo.com>
2019-03-16 18:08:14 -0400

    Updated MainActivity navigation

--- BullyMe/app/src/main/java/net/somee/bullyme/MainActivity.java
+++ BullyMe/app/src/main/java/net/somee/bullyme/MainActivity.java
@@ -1,10 +1,10 @@
 package net.somee.bullyme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -95,13 +95,21 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_questionnaire) {
-
-        } else if (id == R.id.nav_schedule) {
-
+                Intent intent = new Intent(this, Questionnaire.class);
+                startActivity(intent);
+                this.finish();
+            } else if (id == R.id.nav_questionnaire) {
+                Intent intent = new Intent(this, Questionnaire.class);
+                startActivity(intent);
+                this.finish();
+            } else if (id == R.id.nav_schedule) {
+                Intent intent = new Intent(this, Questionnaire.class);
+                startActivity(intent);
+            this.finish();
         } else if (id == R.id.nav_stop) {
-
+            Intent intent = new Intent(this, Questionnaire.class);
+            startActivity(intent);
+            this.finish();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
--- BullyMe/app/src/main/java/net/somee/bullyme/Questionnaire.java
+++ BullyMe/app/src/main/java/net/somee/bullyme/Questionnaire.java
@@ -3,11 +3,9 @@ package net.somee.bullyme;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Questionnaire extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +14,7 @@ public class Questionnaire extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //TODO: Next Button
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
