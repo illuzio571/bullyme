aa9c740c878d6f4c6428a9bf6dcff3f8fd2b2ee7
illuzio <illuzio571@yahoo.com>
2019-03-16 18:12:46 -0400

    Fixed Questionnaire Class

--- BullyMe/app/src/main/java/net/somee/bullyme/Questionnaire.java
+++ BullyMe/app/src/main/java/net/somee/bullyme/Questionnaire.java
@@ -3,14 +3,16 @@ package net.somee.bullyme;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+public class Questionnaire extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_questionnaire);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
