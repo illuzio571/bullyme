9fb1c9534e4a01e6840698aa61ed0e74f3ccd588
illuzio <illuzio571@yahoo.com>
2019-03-16 18:54:01 -0400

    Fixed navigation fully

--- BullyMe/app/src/main/java/net/somee/bullyme/Questionnaire.java
+++ BullyMe/app/src/main/java/net/somee/bullyme/Questionnaire.java
@@ -1,13 +1,19 @@
 package net.somee.bullyme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
-public class Questionnaire extends AppCompatActivity {
+public class Questionnaire extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,15 @@ public class Questionnaire extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //TODO: Next Button
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -25,6 +40,34 @@ public class Questionnaire extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            this.finish();
+        } else if (id == R.id.nav_questionnaire) {
+            Intent intent = new Intent(this, Questionnaire.class);
+            startActivity(intent);
+            this.finish();
+        } else if (id == R.id.nav_schedule) {
+            Intent intent = new Intent(this, Questionnaire.class);
+            startActivity(intent);
+            this.finish();
+        } else if (id == R.id.nav_stop) {
+            Intent intent = new Intent(this, Questionnaire.class);
+            startActivity(intent);
+            this.finish();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
